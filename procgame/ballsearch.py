0feb7cf808391683455f05093945ddb93ea7994f
gstellenberg <gstellenberg@gmail.com>
2009-08-09 18:40:18 -0500

    Added service.py and ballsearch.py files.

---
+++ procgame/ballsearch.py
@@ -0,0 +1,56 @@
+from game import *
+from dmd import *
+
+class BallSearch(Mode):
+	"""Ball Search mode."""
+	def __init__(self, game, priority, countdown_time, reset_switch_names, disable_switch_names=[], enable_switch_names=[], coils=[], special_handler_modes=[]):
+		self.countdown_time = countdown_time
+		self.coils = coils
+		self.special_handler_modes = special_handler_modes
+		Mode.__init__(self, game, 8)
+		for switch in reset_switch_names:
+			self.add_switch_handler(name=switch, event_type='open', delay=None, handler=self.reset)
+		# The disable_switch_names identify the switches that, when closed, 
+		# keep the ball search from occuring.  This is typically done, 
+		# for instance, when a ball is in the shooter lane or held on a flipper.
+		for switch in disable_switch_names:
+			self.add_switch_handler(name=switch, event_type='closed', delay=None, handler=self.stop)
+
+	def sw_trough1_open_for_200ms(self, sw):
+		if self.game.trough_is_full():
+			for special_handler_mode in self.special_handler_modes:
+				special_handler_mode.mode_stopped()
+			self.stop(0)
+
+        def reset(self,sw):
+		self.cancel_delayed(name='ball_search_countdown');
+		self.delay(name='ball_search_countdown', event_type=None, delay=self.countdown_time, handler=self.perform_search, param=0)
+		#return True
+
+        def stop(self,sw):
+		self.cancel_delayed(name='ball_search_countdown');
+
+	def perform_search(self, completion_wait_time, completion_handler = None):
+		if (completion_wait_time != 0):
+			self.game.set_status("Balls Missing") # Replace with permanent message
+		delay = .150
+		for coil in self.coils:
+			self.delay(name='ball_search_coil1', event_type=None, delay=delay, handler=self.pop_coil, param=coil)
+			delay = delay + .150
+		for special_handler_mode in self.special_handler_modes:
+			self.game.modes.add(special_handler_mode)
+			self.delay(name='remove_special_handler_mode', event_type=None, delay=7, handler=self.remove_special_handler_mode, param=special_handler_mode)
+			delay = delay + .150
+
+		if (completion_wait_time != 0):
+			#self.delay(name='search_completion', event_type=None, delay=completion_wait_time, handler=completion_handler, param=completion_param)
+			pass
+		else:
+			self.cancel_delayed(name='ball_search_countdown');
+			self.delay(name='ball_search_countdown', event_type=None, delay=self.countdown_time, handler=self.perform_search, param=0)
+
+	def pop_coil(self,coil):
+		coil.pulse()
+
+	def remove_special_handler_mode(self,special_handler_mode):
+		self.game.modes.remove(special_handler_mode)
---
+++ procgame/service.py
@@ -0,0 +1,153 @@
+from game import *
+from dmd import *
+
+class ServiceModeSkeleton(Mode):
+	"""Service Mode List base class."""
+	def __init__(self, game, priority, font):
+		super(ServiceModeSkeleton, self).__init__(game, priority)
+		self.name = ""
+		self.title_layer = TextLayer(1, 1, font, "left")
+		self.item_layer = TextLayer(128/2, 12, font, "center")
+		self.instruction_layer = TextLayer(1, 25, font, "left")
+		self.layer = GroupedLayer(128, 32, [self.title_layer, self.item_layer, self.instruction_layer])
+
+	def mode_started(self):
+		self.title_layer.set_text(str(self.name))
+		self.game.sound.play('service_enter')
+		self.game.dmd.layers.append(self.layer)
+
+	def mode_stopped(self):
+		self.game.sound.play('service_exit')
+		self.game.dmd.layers.remove(self.layer)
+
+	def sw_exit_closed(self,sw):
+		self.game.modes.remove(self)
+		return True
+
+	def disable(self):
+		pass
+
+class ServiceModeList(ServiceModeSkeleton):
+	"""Service Mode List base class."""
+	def __init__(self, game, priority, font):
+		super(ServiceModeList, self).__init__(game, priority, font)
+		self.items = []
+
+	def mode_started(self):
+		super(ServiceModeList, self).mode_started()
+
+		self.iterator = 0
+		self.change_item()
+
+	def change_item(self):
+		ctr = 0
+                for item in self.items:
+			if (ctr == self.iterator):
+				self.item = item
+			ctr += 1
+		self.max = ctr - 1
+		self.item_layer.set_text(self.item.name)
+
+	def sw_up_closed(self,sw):
+		self.item.disable()
+		if (self.iterator < self.max):
+			self.iterator += 1
+		self.game.sound.play('service_next')
+		self.change_item()
+		return True
+
+	def sw_down_closed(self,sw):
+		self.item.disable()
+		if (self.iterator > 0):
+			self.iterator -= 1
+		self.game.sound.play('service_previous')
+		self.change_item()
+		return True
+
+	def sw_enter_closed(self,sw):
+		return True
+
+	def sw_exit_closed(self,sw):
+		self.item.disable()
+		self.game.modes.remove(self)
+		return True
+
+class ServiceMode(ServiceModeList):
+	"""Service Mode."""
+	def __init__(self, game, priority, font):
+		super(ServiceMode, self).__init__(game, priority,font)
+		self.title_layer.set_text('Service Mode')
+		self.lamp_test = LampTest(self.game, self.priority+1, font)
+		self.coil_test = CoilTest(self.game, self.priority+1, font)
+		self.switch_test = SwitchTest(self.game, self.priority+1, font)
+		self.items = [self.switch_test, self.lamp_test, self.coil_test]
+
+	def sw_enter_closed(self,sw):
+		self.game.modes.add(self.item)
+		return True
+
+class LampTest(ServiceModeList):
+	"""Lamp Test"""
+	def __init__(self, game, priority, font):
+		super(LampTest, self).__init__(game, priority,font)
+		self.name = "Lamp Test"
+		self.items = self.game.lamps
+
+	def change_item(self):
+		super(LampTest, self).change_item()
+		self.item.schedule(schedule=0x00ff00ff, cycle_seconds=0, now=True)
+
+class CoilTest(ServiceModeList):
+	"""Coil Test"""
+	def __init__(self, game, priority, font):
+		super(CoilTest, self).__init__(game, priority, font)
+		self.name = "Coil Test"
+		self.title_layer.set_text('Coil Test - Enter btn: mode')
+		self.instruction_layer.set_text('Pulse with start button')
+		self.items = self.game.coils
+
+	def mode_started(self):
+		super(CoilTest, self).mode_started()
+		self.action = 'manual'
+		self.game.lamps.startButton.schedule(schedule=0xff00ff00, cycle_seconds=0, now=False)
+		self.delay(name='auto', event_type=None, delay=2.0, handler=self.process_auto)
+
+	def process_auto(self):
+		if (self.action == 'auto'):
+			self.item.pulse(20)
+		self.delay(name='auto', event_type=None, delay=2.0, handler=self.process_auto)
+
+
+	def sw_enter_closed(self,sw):
+		if (self.action == 'manual'):
+			self.action = 'auto'
+			self.game.lamps.startButton.disable()
+			self.instruction_layer.set_text('Auto pulse')
+		elif (self.action == 'auto'):
+			self.action = 'manual'
+			self.game.lamps.startButton.schedule(schedule=0xff00ff00, cycle_seconds=0, now=False)
+			self.instruction_layer.set_text('Pulse with start button')
+		return True
+
+	def sw_startButton_closed(self,sw):
+		if (self.action == 'manual'):
+			self.item.pulse(20)
+		return True
+
+class SwitchTest(ServiceModeSkeleton):
+	"""Switch Test"""
+	def __init__(self, game, priority, font):
+		super(SwitchTest, self).__init__(game, priority,font)
+		self.name = "Switch Test"
+		for switch in self.game.switches:
+			if (switch != self.game.switches.exit):
+				self.add_switch_handler(name=switch.name, event_type='open', delay=None, handler=self.switch_handler)
+				self.add_switch_handler(name=switch.name, event_type='closed', delay=None, handler=self.switch_handler)
+
+	def switch_handler(self, sw):
+		if (sw.state):
+			self.game.sound.play('service_switch_edge')
+		self.item_layer.set_text(sw.name + ' - ' + str(sw.state))
+		return True
+			
+
