769ecc1bf8b451710809312ffab832e75f1674f9
preble <preble@gmail.com>
2010-08-08 13:29:09 -0400

    Modified sound module to disable itself and ignore all method calls if mixer.init() fails.

--- procgame/sound.py
+++ procgame/sound.py
@@ -8,9 +8,17 @@ import os.path
 
 class SoundController(object):
 	"""Wrapper for pygame sound."""
+	
+	enabled = True
+	
 	def __init__(self, delegate):
 		super(SoundController, self).__init__()
-		mixer.init()
+		try:
+			mixer.init()
+		except Exception, e:
+			# The import mixer above may work, but init can still fail if mixer is not fully supported.
+			self.enabled = False
+			print("pygame mixer init failed; sound will be disabled: "+str(e))
 		self.sounds = {}
 		self.music = {}
 		self.music_volume_offset = 0
@@ -18,24 +26,29 @@ class SoundController(object):
 
 	def play_music(self, key, loops=0, start_time=0.0):
 		"""Start playing music at the given *key*."""
+		if not self.enabled: return
 		if key in self.music:
 			self.load_music(key)
 			mixer.music.play(loops,start_time)
 
 	def stop_music(self):
 		"""Stop the currently-playing music."""
+		if not self.enabled: return
 		mixer.music.stop()
 
 	def fadeout_music(self, time_ms = 750):
 		""" """
+		if not self.enabled: return
 		mixer.music.fadeout(time_ms)
 
 	def load_music(self, key):
 		""" """
+		if not self.enabled: return
 		mixer.music.load(self.music[key])
 
 	def register_sound(self, key, sound_file):
 		""" """
+		if not self.enabled: return
 		if os.path.isfile(sound_file):
 			self.new_sound = mixer.Sound(str(sound_file))
                		self.sounds[key] = self.new_sound
@@ -45,6 +58,7 @@ class SoundController(object):
 
 	def register_music(self, key, music_file):
 		""" """
+		if not self.enabled: return
 		if os.path.isfile(music_file):
                 	self.music[key] = music_file
 		else:
@@ -52,34 +66,40 @@ class SoundController(object):
 
 	def play(self,key, loops=0, max_time=0, fade_ms=0):
 		""" """
+		if not self.enabled: return
 		if key in self.sounds:
 			self.sounds[key].play(loops,max_time,fade_ms)
 
 	def stop(self,key, loops=0, max_time=0, fade_ms=0):
 		""" """
+		if not self.enabled: return
 		if key in self.sounds:
 			self.sounds[key].stop()
 
 	def volume_up(self):
 		""" """
+		if not self.enabled: return
 		if (self.volume < 0.8):
 			self.set_volume(self.volume + 0.1)
 		return self.volume*10
 
 	def volume_down(self):
 		""" """
+		if not self.enabled: return
 		if (self.volume > 0.2):
 			self.set_volume(self.volume - 0.1)
 		return self.volume*10
 
 	def set_volume(self, new_volume):
 		""" """
+		if not self.enabled: return
 		self.volume = new_volume
 		mixer.music.set_volume (new_volume + self.music_volume_offset)
 		for key in self.sounds:
 			self.sounds[key].set_volume(self.volume)
 
 	def beep(self):
+		if not self.enabled: return
 		pass
 	#	self.play('chime')
 
