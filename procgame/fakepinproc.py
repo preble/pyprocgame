46eac4fce303c16782b69cebda4b4abf4e68a682
preble <preble@gmail.com>
2010-08-08 13:27:59 -0400

    Added FakePinPROC.

---
+++ procgame/fakepinproc.py
@@ -0,0 +1,33 @@
+import time
+import pinproc
+
+class FakePinPROC(object):
+	"""Stand-in class for :class:`pinproc.PinPROC`.  Generates DMD events."""
+	
+	last_dmd_event = 0
+	
+	frames_per_second = 60
+	"""Frames per second at which to dispatch :attr:`pinproc.EventTypeDMDFrameDisplayed` events."""
+	
+	def __init__(self, machineType):
+		pass
+	def noop(self, *args, **kwargs):
+		pass
+	def switch_get_states(self, *args):
+		return [pinproc.EventTypeSwitchOpenDebounced] * 256
+	def get_events(self):
+		events = []
+		now = time.time()
+		seconds_since_last_dmd_event = now - self.last_dmd_event
+		missed_dmd_events = min(int(seconds_since_last_dmd_event*float(self.frames_per_second)), 16)
+		if missed_dmd_events > 0:
+			self.last_dmd_event = now
+			events.extend([{'type':pinproc.EventTypeDMDFrameDisplayed, 'value':0}] * missed_dmd_events)
+		return events
+	def __getattr__(self, name):
+		if name == 'get_events':
+			return self.get_events
+		elif name == 'switch_get_states':
+			return self.switch_get_states
+		else:
+			return self.noop
