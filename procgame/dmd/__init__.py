91a6440f4b3fb35e625414e2286384289f0b732e
preble <preble@gmail.com>
2010-10-03 00:41:42 -0400

    Split up dmd submodule.

--- procgame/dmd/__init__.py
+++ procgame/dmd/__init__.py
@@ -1,4 +1,12 @@
 __all__ = [
+	'displaycontroller'
 	'dmd',
+	'layers',
+	'markup',
+	'transitions',
 	]
+from displaycontroller import *
 from dmd import *
+from layers import *
+from markup import *
+from transitions import *
