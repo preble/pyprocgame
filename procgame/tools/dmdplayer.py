3b3ea9063e86e421d713e2504b5b0aa306232b06
preble <preble@gmail.com>
2011-01-26 01:06:13 -0500

    Rewrote dmdplayer to run off of BasicGame.

--- procgame/tools/dmdplayer.py
+++ procgame/tools/dmdplayer.py
@@ -1,36 +1,38 @@
 import sys
 import os
 import pinproc
-from procgame import dmd
+import procgame.game
+import procgame.dmd
 import time
+import logging
 
-# dmdplayer.py demonstrates how to load and display a sequence of DMD frames.
+logging.basicConfig(level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
-def play(filename):
-	anim = dmd.Animation().load(filename)
-	if anim.width != 128 or anim.height != 32:
-	    print("Dimentions are %dx%d; expected 128x32." % (anim.width, anim.height))
-	    return False
+class PlayerGame(procgame.game.BasicGame):
 	
-	proc = pinproc.PinPROC(pinproc.MachineTypeCustom)
-	layer = dmd.AnimatedLayer(frames=anim.frames, repeat=False, hold=False)
-	while True:
-		for event in proc.get_events():
-			if event['type'] == pinproc.EventTypeDMDFrameDisplayed:
-				frame = layer.next_frame()
-				if frame == None:
-					sys.exit(0)
-				proc.dmd_draw(frame)
-				if len(layer.frames) == 0:
-					proc.dmd_draw(frame)
-					proc.dmd_draw(frame)
-					proc.dmd_draw(frame)
+	anim_layer = None
 	
-	return True
+	def __init__(self, machine_type):
+		super(PlayerGame, self).__init__(machine_type)
+		self.anim_layer = procgame.dmd.AnimatedLayer()
+		mode = procgame.game.Mode(game=self, priority=1)
+		mode.layer = self.anim_layer
+		self.modes.add(mode)
+	
+	def play(self, filename, repeat):
+		anim = procgame.dmd.Animation().load(filename)
+		self.anim_layer.frames = anim.frames
+		self.anim_layer.repeat = repeat
+		if not repeat:
+			self.anim_layer.add_frame_listener(-1, self.end_of_animation)
+	
+	def end_of_animation(self):
+		self.end_run_loop()
 
 
 def tool_populate_options(parser):
-    pass
+	parser.add_option('-m', '--machine-type', action='store', help='wpc, wpc95, stermSAM, sternWhitestar or custom (default)')
+	parser.add_option('-r', '--repeat', action='store_true', help='Repeat the animation indefinitely')
 
 def tool_get_usage():
     return """<file.dmd>"""
@@ -38,4 +40,16 @@ def tool_get_usage():
 def tool_run(options, args):
 	if len(args) != 1:
 		return False
-	return play(filename=args[0])
+	
+	if options.machine_type:
+		machine_type = pinproc.normalize_machine_type(options.machine_type)
+	else:
+		machine_type = pinproc.MachineTypeCustom
+	
+	game = PlayerGame(machine_type=machine_type)
+	
+	game.play(filename=args[0], repeat=options.repeat)
+	
+	game.run_loop()
+	del game
+	return True
